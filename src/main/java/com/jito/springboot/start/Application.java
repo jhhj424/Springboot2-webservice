5e80956c4e15fafcabcf6df7acbcc1d32d1cfe3d
jhhj424 <jhhj424@naver.com>
2020-06-28 18:23:06 +0900

    20200628 18:21  여기까지))   - 스프링 시큐리티를 이용한 구글/네이버 로그인 연동 방법   - 세션 저장소로 톰캣/ 데이터베이스/ 메모리 DB가 있는데 이 중 데이터베이스로 세션 저장소 설정   - ArgumentResolver를 이용하여 어노테이션으로 로그인 세션 정보 가져오기!   - 스프링 시큐리티 적용 시 기존 테스트 코드에서 문제 해결 방법

--- src/main/java/com/jito/springboot/start/Application.java
+++ src/main/java/com/jito/springboot/start/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing // JPA Audiding 활성화 (JPA Audiding : 시간에 대해서 자동으로 값을 넣어주는 기능) - 사용하기 위해선 최소 하나의 @Entity 클래스가 필요함
+//@EnableJpaAuditing // JPA Audiding 활성화 (JPA Audiding : 시간에 대해서 자동으로 값을 넣어주는 기능) - 사용하기 위해선 최소 하나의 @Entity 클래스가 필요함!
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
