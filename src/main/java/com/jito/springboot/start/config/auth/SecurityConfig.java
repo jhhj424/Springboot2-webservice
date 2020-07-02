0d009e0fe74b73ce33f062209eebb4647156aab3
jhhj424 <jhhj424@naver.com>
2020-07-02 23:18:55 +0900

    배포 시에 8081포트를 쓸지, 8082포트를 쓸지 판단하는 기준이 되는 API 추가함

--- src/main/java/com/jito/springboot/start/config/auth/SecurityConfig.java
+++ src/main/java/com/jito/springboot/start/config/auth/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .authorizeRequests() // URL 별 권한 관리를 설정하는 옵션의 시작점입니다. authrizeRequests가 선언되어야만 antMatchers옵션을 사용할 수 있음
                     .antMatchers("/","/css/**","/images/**",
-                            "/js/**","/h2-console/**").permitAll()
+                            "/js/**","/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // 권한 관리 대상을 지정하는 옵션, URL, HTTP 메소드별로 관리가 가능함, "/"등 지정된 URL들은 permitAll() 옵션을 통해 전체 열람 권한을 줌, "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 함
                     .anyRequest().authenticated() // 설정된 값들 이외의 나머지 URL들을 나타냄, 여기서는  authenticated()을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용하게 함, 인증된 사용자 즉, 로그인한 사용자들을 이야기함
                 .and()
