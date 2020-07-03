7cac682ddfbe2732516de5a5c3e89f05a9deae2d
jhhj424 <jhhj424@naver.com>
2020-07-04 02:46:17 +0900

    main 화면 수정  - main.mustache 파일 추가  - IndexController 에서 첫 화면 index -> main으로 변경  - 처음 로그인 하는 유저의 Role을 GUEST -> USER로 변경

--- src/main/java/com/jito/springboot/start/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jito/springboot/start/config/auth/dto/OAuthAttributes.java
@@ -56,13 +56,13 @@ public class OAuthAttributes {
                 .build();
     }
 
-    // User 엔티티를 생성함, OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때임, 가입할 떄의 기본 권한을 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용함
+    // User 엔티티를 생성함, OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때임, 가입할 떄의 기본 권한을 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용함(200704:처음에 USER로 설정)
     public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
